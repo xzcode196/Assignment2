3fac9123418a84e375fe5436988b2de380a1cb27
xzcode196 <xzhang48@charlotte.edu>
2026-02-13 23:27:36 -0500

    Added code to both check_recources and make_sandwich function

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -4,7 +4,13 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                return False
+            return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+            print(f"{sandwich_size} sandwich is ready. Bon appetit!")
